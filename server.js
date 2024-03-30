2fd8df33bd847eb843b7a6e67b1832cba5a74848
Maheboobkhan <maheboobkhan18202@gmail.com>
2024-03-31 02:55:07 +0530

    second

--- server.js
+++ server.js
@@ -193,7 +193,7 @@ app.delete('/api/news/:id', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
